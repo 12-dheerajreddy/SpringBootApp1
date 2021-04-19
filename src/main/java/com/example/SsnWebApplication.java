bb595ae70bbd6d8b6fb5425d358fcdd7f4d289f6
12-dheerajreddy <dheerajreddy540@gmail.com>
2021-04-19 09:52:14 +0530

    some changes has been happen

--- src/main/java/com/example/SsnWebApplication.java
+++ src/main/java/com/example/SsnWebApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsnWebApplication {
+	Map<String,Integer> ma=new Map<String,Integer>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsnWebApplication.class, args);
+	}
+	
+	public String m1(){
+	
+	System.out.println("this is my m1 method");
 	}
 
 }
