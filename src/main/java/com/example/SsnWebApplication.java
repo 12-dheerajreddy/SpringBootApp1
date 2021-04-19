ae743532191b75c9ae69c8b30ad13e6964616ee5
12-dheerajreddy <dheerajreddy540@gmail.com>
2021-04-19 09:42:44 +0530

    data commited

--- src/main/java/com/example/SsnWebApplication.java
+++ src/main/java/com/example/SsnWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsnWebApplication {
+	private final static String req="https://facebook.come";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsnWebApplication.class, args);
